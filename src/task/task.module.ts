64ec635894fa814a174a88ac7722231694373d37
RubenLazarus <rubenlazarus19@gmail.com>
2024-02-10 01:12:08 +0530

    cron done

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -4,9 +4,10 @@ import { TaskService } from './task.service';
 import { Services } from 'src/utils/constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import entities from 'src/utils/entities';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports:[TypeOrmModule.forFeature(entities)],
+  imports:[TypeOrmModule.forFeature(entities),ScheduleModule.forRoot()],
   controllers: [TaskController],
   providers: [{
     provide:Services.TASK,
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskEntity } from 'src/entities/task.entity';
 import { Status } from 'src/utils/constants';
 import { filterData } from 'src/utils/types';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 
 @Injectable()
 export class TaskService {
@@ -52,7 +53,7 @@ export class TaskService {
             if (filterData?.pageSize) {
                 pageSize = filterData.pageSize;
             }
-            let object = Object.assign({},...search)
+            let object = Object.assign({}, ...search)
             const taskCount = await this.taskRepository
                 .count({ where: object })
             var numberOfPages = pageSize === 0 ? 1 : Math.ceil(taskCount / pageSize);
@@ -91,14 +92,14 @@ export class TaskService {
             if (userId) {
                 search.push({ assignedUser: { id: userId } })
             }
-            let object= Object.assign({},...search);
+            let object = Object.assign({}, ...search);
             const taskCount = await this.taskRepository
                 .count({ relations: ['assignedUser'], where: object })
             var numberOfPages = pageSize === 0 ? 1 : Math.ceil(taskCount / pageSize);
 
             const allTask = await this.taskRepository.find({
                 relations: ['assignedUser'],
-                where:object,
+                where: object,
                 order: {
                     createdAt: 'DESC'
                 },
@@ -116,153 +117,192 @@ export class TaskService {
             console.log(e)
         }
     }
-    async getTaskById(taskId:any){
+    async getTaskById(taskId: any) {
         try {
 
-            let task = await this.taskRepository.findOne({where:taskId})
-            if(!task){
+            let task = await this.taskRepository.findOne({ where: taskId })
+            if (!task) {
                 return {
-                    success :false,
-                    message:"Task not found"
+                    success: false,
+                    message: "Task not found"
                 }
             }
             return {
-                success:true,
-                message:"Task Found successfully",
-                taskDetails:task
+                success: true,
+                message: "Task Found successfully",
+                taskDetails: task
             }
         } catch (e) {
             console.log(e)
         }
     }
-    async updateTask(taskData:any){
+    async updateTask(taskData: any) {
         try {
-            if(!taskData?.id){
+            if (!taskData?.id) {
                 return {
-                    success:false,
-                    message:"Id not found please send id in body"
+                    success: false,
+                    message: "Id not found please send id in body"
                 }
             }
-            let getTask = await this.taskRepository.findOneBy({id:taskData?.id})
-            if(taskData?.title){
-                getTask.title=taskData?.title
+            let getTask = await this.taskRepository.findOneBy({ id: taskData?.id })
+            if (taskData?.title) {
+                getTask.title = taskData?.title
             }
-            if(taskData?.projectName){
-                getTask.projectName=taskData?.projectName
+            if (taskData?.projectName) {
+                getTask.projectName = taskData?.projectName
             }
-            if(taskData?.assignedUser){
-                getTask.assignedUser=taskData?.assignedUser
+            if (taskData?.assignedUser) {
+                getTask.assignedUser = taskData?.assignedUser
             }
-            if(taskData?.prioriyt){
-                getTask.prioriyt=taskData?.prioriyt
+            if (taskData?.prioriyt) {
+                getTask.prioriyt = taskData?.prioriyt
             }
-            if(taskData?.descreption){
-                getTask.descreption=taskData?.descreption
+            if (taskData?.descreption) {
+                getTask.descreption = taskData?.descreption
             }
-            if(taskData?.status){
-                getTask.status=taskData?.status
+            if (taskData?.status) {
+                getTask.status = taskData?.status
             }
             let updataTask = await this.taskRepository.save(getTask)
-            if(!updataTask){
+            if (!updataTask) {
                 return {
-                    success:false,
-                    message:"Unable to update"
+                    success: false,
+                    message: "Unable to update"
                 }
             }
 
             return {
-                success:true,
+                success: true,
                 message: "Updated successfully",
-                taskDetails:updataTask
+                taskDetails: updataTask
             }
         } catch (e) {
             console.log(e)
         }
     }
-    async deleteTask (taskId){
-        if(!taskId){
-            return{
-                success:false,
-                message:"Please provid id"
+    async deleteTask(taskId) {
+        if (!taskId) {
+            return {
+                success: false,
+                message: "Please provid id"
             }
         }
-        
-        let getTask = await this.taskRepository.findOneBy({id:taskId});
-        if(!getTask){
+
+        let getTask = await this.taskRepository.findOneBy({ id: taskId });
+        if (!getTask) {
             return {
-                success:false,
+                success: false,
                 message: "Task not found"
             }
         }
         getTask.isDeleted = !getTask.isDeleted
         let deleted = await this.taskRepository.save(getTask)
-        if(!deleted){
+        if (!deleted) {
             return {
-                success:false,
+                success: false,
                 message: "Unable to Delete Task"
             }
         }
         return {
-            success:true,
-            message :"Task has been deleted successfully"
+            success: true,
+            message: "Task has been deleted successfully"
         }
 
     }
-    async activeInaciveTask (taskId){
-        if(!taskId){
-            return{
-                success:false,
-                message:"Please provid id"
+    async activeInaciveTask(taskId) {
+        if (!taskId) {
+            return {
+                success: false,
+                message: "Please provid id"
             }
         }
-        
-        let getTask = await this.taskRepository.findOneBy({id:taskId});
-        if(!getTask){
+
+        let getTask = await this.taskRepository.findOneBy({ id: taskId });
+        if (!getTask) {
             return {
-                success:false,
+                success: false,
                 message: "Task not found"
             }
         }
         getTask.isActive = !getTask?.isActive
         let deleted = await this.taskRepository.save(getTask)
-        if(!deleted){
+        if (!deleted) {
             return {
-                success:false,
+                success: false,
                 message: "Unable to change Active status"
             }
         }
         return {
-            success:true,
-            message :"Task has been updated successfully"
+            success: true,
+            message: "Task has been updated successfully"
         }
 
     }
-    async hardDeleteTask (taskId){
-        if(!taskId){
-            return{
-                success:false,
-                message:"Please provid id"
+    async hardDeleteTask(taskId) {
+        if (!taskId) {
+            return {
+                success: false,
+                message: "Please provid id"
             }
         }
-        
-        let getTask = await this.taskRepository.findOneBy({id:taskId});
-        if(!getTask){
+
+        let getTask = await this.taskRepository.findOneBy({ id: taskId });
+        if (!getTask) {
             return {
-                success:false,
+                success: false,
                 message: "Task not found"
             }
         }
-        let deleted = await this.taskRepository.delete({id:getTask?.id})
-        if(!deleted){
+        let deleted = await this.taskRepository.delete({ id: getTask?.id })
+        if (!deleted) {
             return {
-                success:false,
+                success: false,
                 message: "Unable to Delete Task"
             }
         }
         return {
-            success:true,
-            message :"Task has been Hard Deleted successfully"
+            success: true,
+            message: "Task has been Hard Deleted successfully"
         }
 
+    }
+    @Cron('*/10 * * * *')
+    async hendlearTaskCron() {
+        try {
+            let getalltask = await this.taskRepository.find({ where: { status: Not(Status.RESOLVED) } })
+            if (getalltask && getalltask.length > 0) {
+                for await (const iterator of getalltask) {
+                    let currentData = new Date()
+                    let createdAt = new Date(iterator?.createdAt)
+                    const taskTime = Math.floor((currentData.getTime() - createdAt.getTime()))
+                    const showTime = this.parseMillisecondsIntoReadableTime(taskTime)
+                    await this.taskRepository.update({ id: iterator?.id }, { timeTaken: taskTime, showTimeTaken: showTime })
+
+                }
+            }
+
+        } catch (e) {
+            console.log(e)
+
+        }
+    }
+    private parseMillisecondsIntoReadableTime(milliseconds) {
+        //Get hours from milliseconds
+        var hours = milliseconds / (1000 * 60 * 60);
+        var absoluteHours = Math.floor(hours);
+        var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+        //Get remainder from hours and convert to minutes
+        var minutes = (hours - absoluteHours) * 60;
+        var absoluteMinutes = Math.floor(minutes);
+        var m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
+
+        //Get remainder from minutes and convert to seconds
+        var seconds = (minutes - absoluteMinutes) * 60;
+        var absoluteSeconds = Math.floor(seconds);
+        var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+
+
+        return h + ':' + m + ':' + s;
     }
 }
